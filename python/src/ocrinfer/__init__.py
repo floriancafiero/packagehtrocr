05f39c6701a6727a0f2562d1fa0dbe1d32a2696b
Florian Cafiero <florian.cafiero@polytechnique.edu>
2026-09-18 17:06:37 +0200

    Add Python ocrinfer package skeleton and core metrics

---
+++ python/src/ocrinfer/__init__.py
@@ -0,0 +1,37 @@
+from .core import (
+    AlignmentRow,
+    EditCounts,
+    align_text,
+    cer,
+    edit_counts,
+    normalize_text,
+    wer,
+)
+from .evaluation import (
+    evaluate_recognition,
+    normalization_sensitivity,
+    summarize_recognition,
+    summarise_recognition,
+)
+from .compare import compare_systems
+from .diagnostics import extract_error_spans, error_profile, confusion_table
+from .policy import policy_swap_summary
+
+__all__ = [
+    "AlignmentRow",
+    "EditCounts",
+    "align_text",
+    "cer",
+    "edit_counts",
+    "normalize_text",
+    "wer",
+    "evaluate_recognition",
+    "normalization_sensitivity",
+    "summarize_recognition",
+    "summarise_recognition",
+    "compare_systems",
+    "extract_error_spans",
+    "error_profile",
+    "confusion_table",
+    "policy_swap_summary",
+]
